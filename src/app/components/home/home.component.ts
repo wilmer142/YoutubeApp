4f633bee94980ef8cb2d3be7eb96035885138b94
wilmer142 <wilmercollege@gmail.com>
2019-11-24 16:54:34 -0500

    Creating modal and refactor service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtube.service';
+
+declare var $:any;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  videos: any[] = [];
+  videoSel:any;
+  constructor(private youtubeService: YoutubeService) {
+    this.youtubeService.getVideos()
+      .subscribe( videos => {
+        this.videos = videos;
+      });
+  }
 
   ngOnInit() {
+  }
+
+  verVideo(video:any){
+    this.videoSel = video;
+    $('#exampleModal').modal();
   }
 
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyD6BGIWEPI3XXpBRhVl3sjgwulthCih3Wk';
+  private playListId = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken;
+  constructor(public httpClient: HttpClient) { }
+
+  getVideos() {
+    const url = `${this.youtubeUrl}/playlistItems?part=snippet&maxResults=10&playlistId=${this.playListId}&key=${this.apiKey}`;
+    // const url = `${this.youtubeUrl}/playlistItems`;
+
+    const params = new HttpParams();
+    params.set('part', 'snippet');
+    params.set('maxResults', '10');
+    params.set('playlistId', this.playListId);
+    params.set('key', this.apiKey);
+
+    return this.httpClient.get( url )
+      .pipe(
+        map( (data: any) => {
+          this.nextPageToken = data.nextPageToken;
+
+          let videos: any[] = [];
+          for(let video of data.items) {
+            let snippet =  video.snippet;
+            videos.push(snippet);
+          }
+          
+          return videos;
+        })
+      );
+  }
 }
